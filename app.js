99217698285030c665da0d5aa3fa2be4f37ea899
Darunia <maximevaneygen@gmail.com>
2023-03-27 14:10:02 +0200

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var studentRouter = require('./routes/student');
@@ -24,5 +25,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/student', studentRouter);
+app.use(cors());
 
 module.exports = app;
